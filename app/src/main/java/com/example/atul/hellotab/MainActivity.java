69416c58e3c5ef8de05737d1739f1a79b2a56f73
atul <atul080796@gmail.com>
2018-04-22 18:05:40 +0530

    First Commit

---
+++ app/src/main/java/com/example/atul/hellotab/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.atul.hellotab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.widget.Toast;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
